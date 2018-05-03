19b14c7820d833f0819b2ae09c97c1a43ac96956
vishnum360 <vishnu.m360@gmail.com>
2018-05-03 20:36:17 +0530

    commit

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { TabsPage } from '../tabs/tabs';
+import { ForgotPassPage } from '../forgot-pass/forgot-pass';
 /**
  * Generated class for the LoginPage page.
  *
@@ -10,16 +11,24 @@ import { TabsPage } from '../tabs/tabs';
 
 @IonicPage()
 @Component({
-  selector: 'page-login',
-  templateUrl: 'login.html',
+  selector: "page-login",
+  templateUrl: "login.html"
 })
 export class LoginPage {
-
+  public formData: any;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.formData = {};
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad LoginPage');
+    console.log("ionViewDidLoad LoginPage");
+  }
+  login(){
+
   }
 
+  // GO TO FORGOT PASSWORD PAGE
+  goToForgotPass(){
+    this.navCtrl.push(ForgotPassPage);
+  }
 }
