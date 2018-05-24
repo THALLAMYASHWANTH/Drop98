e32074711c2d8fc7a3524e6907dd5bf2f5058fa7
THALLAMYASHWANTH <thallamyashwanth@gmail.com>
2018-05-24 12:56:10 +0530

    final commit

--- src/pages/profilepage/profilepage.ts
+++ src/pages/profilepage/profilepage.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Storage } from "@ionic/storage";
 import { DashboardPage } from '../dashboard/dashboard';
+import { RemoteServiceProvider } from './../../providers/remote-service/remote-service';
 
 /**
  * Generated class for the ProfilepagePage page.
@@ -22,28 +23,29 @@ export class ProfilepagePage {
 swipe(){
   this.navCtrl.pop();
 }
-  constructor(public navCtrl: NavController, public navParams: NavParams, private storage: Storage) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private storage: Storage,public remoteService: RemoteServiceProvider) {
     this.getInfo={};
     this.edit = false;
-    storage.get('user').then((val) => {
-      this.getInfo.name = val;
-    });
-    storage.get("logo").then(val => {
-      this.getInfo.userphoto = val;
-      console.log(val);
-    });
-    storage.get("email").then(val => {
-      this.getInfo.email = val;
-    });
-    storage.get("auth").then(val => {
-      this.getInfo.loggedin = val;
-    });
-    storage.get("custtype").then(val => {
-      this.getInfo.privilege = val;
-    });
+    this.storage.get("token").then(val => {
+      let token = val;
+      this.storage.get("custid").then(val => {
+        let url = "https://www.dbdwater.com/smartmeter_webapp/api/rest/managecustomers/getCustomers/" + val;
 
-    
+
+        this.remoteService.getPosts(url, token).subscribe((data) => {
+
+          console.log(data);
+          this.getInfo.firstname=data.firstName;
+          this.getInfo.lastname = data.lastName;
+          this.getInfo.username = data.name;
+          this.getInfo.email = data.email;
+        });
+      });
+    });
   }
+  
+    
+  
 
   ionViewDidLoad() {
     console.log(this.getInfo.name+"  "+this.getInfo.userphoto+ "  "+this.getInfo.email+ "  "+this.getInfo.loggedin);
